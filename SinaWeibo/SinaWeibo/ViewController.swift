92c41f01c66d80166ca94443121ae384c029a07e
DerrickQin <si_yuan_qin@163.com>
2016-09-22 18:48:59 +0800

    初始化项目

---
+++ SinaWeibo/SinaWeibo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SinaWeibo
+//
+//  Created by admin on 16/9/22.
+//  Copyright © 2016年 Derrick_Qin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
