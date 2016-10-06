0b3a31074ca5bb5b820d459e1be986e0b100290a
DerrickQin <si_yuan_qin@163.com>
2016-10-07 00:09:59 +0800

    完成底部tabbar按钮点击的动画

--- SinaWeibo/SinaWeibo/Classes/View/Main/ViewController/DQTabBarController.swift
+++ SinaWeibo/SinaWeibo/Classes/View/Main/ViewController/DQTabBarController.swift
@@ -26,18 +26,20 @@ class DQTabBarController: UITabBarController {
     }
 
     private func addChildViewControllers() {
-        addChildViewController(viewController: DQHomeViewController(), title: "首页", imageName: "tabbar_home")
-        addChildViewController(viewController: DQMessageViewController(), title: "消息", imageName: "tabbar_message_center")
-        addChildViewController(viewController: DQDiscoverViewController(), title: "发现", imageName: "tabbar_discover")
-        addChildViewController(viewController: DQProfileViewController(), title: "我", imageName: "tabbar_profile")
+        addChildViewController(viewController: DQHomeViewController(), title: "首页", imageName: "tabbar_home",index: 0)
+        addChildViewController(viewController: DQMessageViewController(), title: "消息", imageName: "tabbar_message_center",index: 1)
+        addChildViewController(viewController: DQDiscoverViewController(), title: "发现", imageName: "tabbar_discover",index: 2)
+        addChildViewController(viewController: DQProfileViewController(), title: "我", imageName: "tabbar_profile",index: 3)
     }
 
-    private func addChildViewController(viewController: UIViewController, title: String, imageName: String) {
+    private func addChildViewController(viewController: UIViewController, title: String, imageName: String, index: Int) {
         //设置title
         viewController.navigationItem.title = title
         
         //BUG:要同时设置title 和图片才可以显示
         viewController.tabBarItem.title = title
+        //添加tag
+        viewController.tabBarItem.tag = index
         viewController.tabBarItem.image = UIImage(named: imageName)?.withRenderingMode(.alwaysOriginal)
         //选中图片
         viewController.tabBarItem.selectedImage = UIImage(named: imageName + "_selected")?.withRenderingMode(.alwaysOriginal)
@@ -55,5 +57,24 @@ class DQTabBarController: UITabBarController {
         
         addChildViewController(navigationVC)
         
+    }
+    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        var index = 0
+        for subview in tabBar.subviews {
+            if subview.isKind(of: NSClassFromString("UITabBarButton")!) {
+                if index == item.tag{
+                    for target in subview.subviews {
+                        if target.isKind(of: NSClassFromString("UITabBarSwappableImageView")!) {
+                            target.transform = CGAffineTransform.init(scaleX: 0.4, y: 0.4)
+                            UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [], animations: { 
+                                target.transform = CGAffineTransform.init(scaleX: 1, y: 1)
+                                }, completion: nil)
+                        }
+                    }
+                }
+                index += 1
+            }
+        }
     }
 }
